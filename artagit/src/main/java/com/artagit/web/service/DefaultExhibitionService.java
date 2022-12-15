b95119008b5464d82550c3ed5b88acd161bdd18d
HoDdoong <ghqkd333@gmail.com>
2022-12-15 18:27:04 +0900

    Exhibition Service (Entity, Service, Controller, Dao) 작성

--- artagit/src/main/java/com/artagit/web/service/DefaultExhibitionService.java
+++ artagit/src/main/java/com/artagit/web/service/DefaultExhibitionService.java
@@ -12,36 +12,32 @@ import com.artagit.web.entity.Exhibition;
 public class DefaultExhibitionService implements ExhibitionService {
 	
 	@Autowired
-	private ExhibitionDao exhibitionDao;
+	private ExhibitionDao exhDao;
 	
+	// 기본생성자
 	public DefaultExhibitionService() {
 
 	}
 	
-	public DefaultExhibitionService(ExhibitionDao exhibitionDao) {
-
-		this.exhibitionDao = exhibitionDao;
+	public DefaultExhibitionService(ExhibitionDao exhDao) {
+		this.exhDao = exhDao;
 	}
 
-	
-	
-
-
 	@Override
+	// 인터페이스를 구현하는 메서드
 	public int reg(Exhibition exhibition) {
-		
-		exhibitionDao.insert(exhibition);
-		
-		// TODO Auto-generated method stub
-		return exhibition.getId();
+		return exhDao.insert(exhibition);
 	}
 
 	@Override
+	// 페이지 지정해서 목록 조회하는 메서드
 	public List<Exhibition> getList(int page) {
 		
-		List<Exhibition> list = exhibitionDao.getList(0, null, null);
+		int size = 6;
+		int offset = (page-1)*size;
+		
+		List<Exhibition> list = exhDao.getList(offset, size);
 		
 		return list;
 	}
-
 }
