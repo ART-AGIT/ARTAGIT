139c5b214f3ac5e5c01364ac7ef9926464a0fce6
sungkyoon <harsol@naver.com>
2022-12-19 16:03:08 +0900

    ignore application.properties

--- artagit/src/main/java/com/artagit/web/service/DefaultMemberService.java
+++ artagit/src/main/java/com/artagit/web/service/DefaultMemberService.java
@@ -1,16 +1,13 @@
 package com.artagit.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
-import org.springframework.stereotype.Component;
->>>>>>> dev
+
 import org.springframework.stereotype.Service;
 
 import com.artagit.web.dao.MemberDao;
 import com.artagit.web.entity.Member;
 
-<<<<<<< HEAD
+
 @Service
 public class DefaultMemberService implements MemberService{
 
@@ -22,18 +19,7 @@ public class DefaultMemberService implements MemberService{
 		int result;
 		result = memberDao.insert(member);
 		return result;
-=======
-@Component
-public class DefaultMemberService implements MemberService{
-	
-	@Autowired
-	private MemberDao memberDao;
 
-	@Override
-	public void insertMember(Member member) {
-		memberDao.insert(member);
-
->>>>>>> dev
 	}
 
 }
--- artagit/src/main/java/com/artagit/web/service/MemberService.java
+++ artagit/src/main/java/com/artagit/web/service/MemberService.java
@@ -2,18 +2,10 @@ package com.artagit.web.service;
 
 import com.artagit.web.entity.Member;
 
-<<<<<<< HEAD
-
 public interface MemberService {
 	
 	int reg(Member member);
 
 //	void insertMember(Member member);
-	
-=======
-public interface MemberService {
-	
-	void insertMember(Member member);
 
->>>>>>> dev
 }
