a67d266955011dcce8a4f148e8a48a74dbf3fa47
kyoumin <cherish__97@naver.com>
2022-12-23 17:11:44 +0900

    corporate>ExhibitionController.java 수정 member>MypageController.java 추가

--- artagit/src/main/java/com/artagit/web/controller/member/MypageController.java
+++ artagit/src/main/java/com/artagit/web/controller/member/MypageController.java
@@ -1,40 +1,57 @@
-package com.artagit.web.controller.member;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.artagit.web.service.ExhibitionService;
-
-
-@Controller
-@RequestMapping("/mypage")
-public class MypageController {
-	
-	@Autowired 
-	private ExhibitionService exhService;
-	
-	
-	//예매내역리스트 
-	@GetMapping("/review/list")
-	public String list() {
-		int memId =1;
-		exhService.getListById(memId);
-		return "member/mypage/booking-list";
-		
-	}
-	
-	
-	
-	
-//	//리뷰등록
-//	@PostMapping("/review/reg")
-//	public String list() {
-//		
-//		
-//		return null;
+//package com.artagit.web.controller.member;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import com.artagit.web.entity.Exhibition;
+//import com.artagit.web.service.ExhibitionService;
+////import com.artagit.web.service.MyPageService;
+//
+//
+//@Controller
+//@RequestMapping("/mypage")
+//public class MypageController {
+//	
+//	@Autowired 
+//	private ExhibitionService exhService;
+//	
+//	@Autowired
+////	private MyPageService myPageService;
+//	
+//	//예매내역리스트 
+//	@GetMapping("/review/list")
+//	public String list(Model model) {
+//		int memId =58;
+//		List<Exhibition> list = exhService.getListById(memId);
+//		model.addAttribute("list",list);
+//		return "member/mypage/booking-list";
 //		
 //	}
-}
+//	
+//	/*
+//	@PostMapping("/review/reg")
+//	public String reg() {
+//		int memId = 58;
+//		myPageService.reg(memId);
+//		return null;
+//	}
+//	*/
+//	
+//	
+//	
+//	
+////	//리뷰등록
+////	@PostMapping("/review/reg")
+////	public String list() {
+////		
+////		
+////		return null;
+////		
+////	}
+//}
