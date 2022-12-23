e7fb8aa95de7dd1f2205dda5ef2d26fe53729a20
lampjin <dlwlsgml3325@gmail.com>
2022-12-23 17:22:59 +0900

    merge

---
+++ artagit/src/main/java/com/artagit/web/dao/BoardDao.java
@@ -0,0 +1,26 @@
+package com.artagit.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.artagit.web.entity.Board;
+
+@Mapper
+public interface BoardDao {
+	
+	List<Board> getList(
+			int offset,
+			int size,
+			Integer categoryId);
+	
+	Board get(int id);
+	
+	int count(String input);
+	
+	int insert(Board board);
+	int update(Board board);
+	int delete(int id);
+	
+
+}
