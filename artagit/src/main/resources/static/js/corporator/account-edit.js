7ad06c5bc78ad16b96fac07e1b5e73af82e2207a
hi0424 <tkdals9903@naver.com>
2022-12-26 16:15:26 +0900

    account-edit.html css js 수정

--- artagit/src/main/resources/static/js/corporator/account-edit.js
+++ artagit/src/main/resources/static/js/corporator/account-edit.js
@@ -2,9 +2,35 @@
  * 
  */
  window.addEventListener("load",function(){
+	//	img 수정
 	 const imgInput = document.querySelector(".img-input");
 	 const fileInput = document.querySelector(".file-input");
+	// toggle  
+	//  var section = this.document.querySelector(".reg-list");
+	//  var regListBox = section.querySelector(".reg-list-box");
+	//  // var headers = section.querySelector(".active"); for문을 돌려서 remove할때
+	//  var current =section.querySelector(".active");
+ 
+	//  // toggle
+	//  regListBox.onclick = function(e){
+	// 	 var isHeader = e.target.nodeName != "H1"
+	// 				 || e.target.classList.contains("accordion-header")
+		 
+	// 	 if(!isHeader)
+	// 		 return;
+ 
+	// 	 if(current !=null)
+	// 		 current.classList.remove("active");
+		 
+	// 	 e.target.classList.add("active");
+	// 	 current = e.target;
+		 
 	 
+	// 	//console.log("test"); 
+
+	//  };
+
+	//  img 수정
 	 imgInput.onclick = function(e){
 		 e.preventDefault();
 		 let event = new MouseEvent("click",{
@@ -28,4 +54,6 @@
 	 }
 	 
  });
+
+
  
