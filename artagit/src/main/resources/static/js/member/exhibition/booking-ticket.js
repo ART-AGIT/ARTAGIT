da7f229faec693fbb0c995ce9a32302216af9677
HoDdoong <ghqkd333@gmail.com>
2022-12-26 18:52:50 +0900

    booking-ticket, (exh)detail 페이지 js 파일 연결

---
+++ artagit/src/main/resources/static/js/member/exhibition/booking-ticket.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(e){
+	const cNumBox = document.querySelector(".client-number");
+	const cInfoBox = document.querySelector(".client-info");
+	// const info
+	const date = document.querySelector(".choice-date");
+	const incOrdec = document.querySelector(".plus");
+
+	date.onclick = function(e){
+		console.log("hihihihihihi");
+		cNumBox.classList.remove("d-none");
+	}
+
+	incOrdec.onclick = function(e){
+		e.preventDefault();
+		cInfoBox.classList.remove("d-none");
+	}
+});
