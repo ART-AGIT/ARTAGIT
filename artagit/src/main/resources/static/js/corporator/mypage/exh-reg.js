7880cce9337e3b83119db19dfae0d2b466449bb1
HB <hbej9607@gmail.com>
2022-12-29 09:19:32 +0900

    exh-reg 화면전환수정

--- artagit/src/main/resources/static/js/corporator/mypage/exh-reg.js
+++ artagit/src/main/resources/static/js/corporator/mypage/exh-reg.js
@@ -3,9 +3,9 @@ window.addEventListener("load", function() {
 	let secondNext = document.querySelector("#btn-y");
 	let btnReg = document.querySelector(".exh-reg-button-box-page3");
 	let page1 = document.querySelector(".page1");
-//	let page2 = document.querySelector(".page2");
-//	let page3 = document.querySelector(".page3");
-	
+	//	let page2 = document.querySelector(".page2");
+	//	let page3 = document.querySelector(".page3");
+
 	//	const btnPage = document.querySelector(".exh-reg-list a");
 	// 	const btnBeforeP2 =	document.querySelector(".btn-before x");
 	// 	const btnNextP2 =	document.querySelector(".btn-next y");
@@ -18,6 +18,8 @@ window.addEventListener("load", function() {
 		document.querySelector(".reg-list-page2").classList.remove("d-none");
 		page1.classList.add("d-none");
 		document.querySelector(".exh-reg-button-box-page2").classList.remove("d-none");
+		document.querySelector("#second-circle").classList.remove("circle-off");
+		document.querySelector("#first-circle").classList.add("circle-off");
 	};
 
 	secondNext.onclick = function(e) {
@@ -27,18 +29,20 @@ window.addEventListener("load", function() {
 		document.querySelector(".exh-reg-button-box-page2").classList.add("d-none");
 		document.querySelector(".exh-reg-button-box-page3").classList.remove("d-none");
 		// document.querySelector(".reg-list-page").classList.add("d-none");
+		document.querySelector("#second-circle").classList.add("circle-off");
+		document.querySelector("#third-circle").classList.remove("circle-off");
 	};
 
 	btnReg.onclick = function(e) {
 		e.preventDefault();
 		document.querySelector(".reg-list-page3").classList.add("d-none");
-		
+
 		document.querySelector(".reg-list-page").classList.remove("d-none");
 		document.querySelector(".exh-reg-button-box-page3").classList.add("d-none");
-		document.querySelector(".exh-reg-button-box-page").classList.remove("d-none");
-		console.log(document.querySelector(".reg-list-page").classList.add("d-none"))
+		//document.querySelector(".exh-reg-button-box-page").classList.remove("d-none");
+		//console.log(document.querySelector(".reg-list-page").classList.add("d-none"))
 	};
-	//============================================================
+	//===================뒤로가기버튼=========================================
 	const firstBefore = document.querySelector("#btn-x");
 	const secondBefore = document.querySelector("#btn-z");
 
@@ -49,8 +53,9 @@ window.addEventListener("load", function() {
 		document.querySelector(".reg-list-page").classList.remove("d-none");
 		document.querySelector(".exh-reg-button-box-page2").classList.add("d-none");
 		//document.querySelector(".exh-reg-button-box-page").classList.remove("d-none");
-		console.log("ch")
-
+		document.querySelector("#third-circle").classList.add("circle-off");
+		document.querySelector("#second-circle").classList.add("circle-off");
+		document.querySelector("#first-circle").classList.remove("circle-off");
 	};
 	secondBefore.onclick = function(e) {
 		e.preventDefault();
@@ -60,8 +65,51 @@ window.addEventListener("load", function() {
 		document.querySelector(".reg-list-page3").classList.add("d-none");
 		document.querySelector(".reg-list-page").classList.add("d-none");
 		// document.querySelector("#first-page-next").classList.add("d-none");
-		console.log("ch")
+		document.querySelector("#first-circle").classList.add("circle-off");
+		document.querySelector("#second-circle").classList.remove("circle-off");
+		document.querySelector("#third-circle").classList.add("circle-off");
 	};
+	//==========================이미지 첨부, 미리보기=================================================
+	const imgInput = document.querySelector(".input-image-button1");
+	const fileInput = document.querySelector("#input-image1");
+	const fileInputDe = document.querySelector("#input-image2");
+	const fileInputDe2 = document.querySelector("#input-image3");
+	
+
+	imgInput.onclick = function(e) {
+		
+			
+			let event = new MouseEvent("click", {
+				'view': window,
+				'bubbles': true,
+				'cancelable': true
+			});
+			
+			fileInput.dispatchEvent(event);
+		
+		console.log(e.target);
+	}
+
+	fileInput.oninput = function(e) {
+		let url = fileInput.files[0];
+		let reader = new FileReader();
+		reader.onload = (evt) => {
+		imgInput.src = evt.target.result;
+		};
+		reader.readAsDataURL(url);
+			
+	}
+	
+
+
+
+
+
+
+
+
+
+
 
 
 });
