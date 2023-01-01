29540cea4d97becd2eaf73127646de6eec4bcfbb
HB <hbej9607@gmail.com>
2023-01-01 22:42:12 +0900

    전시등록기능구현중,,,

--- artagit/src/main/java/com/artagit/web/controller/admin/ExhibitionController.java
+++ artagit/src/main/java/com/artagit/web/controller/admin/ExhibitionController.java
@@ -1,6 +1,24 @@
 package com.artagit.web.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.artagit.web.entity.Exhibition;
+import com.artagit.web.service.ExhibitionService;
+
+//@Controller
+//@RequestMapping("/corporator/mypage")
 public class ExhibitionController {
-
+	
+//	@Autowired
+//	private ExhibitionService service;
+//	
+//	@GetMapping("exh-reg")
+//	public String reg(Exhibition exhibition) {
+//		
+//		return "redirect:exh-list";
+//	}
+	
 }
