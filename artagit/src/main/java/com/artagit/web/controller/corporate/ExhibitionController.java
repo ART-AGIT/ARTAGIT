a31fd37e9eddf0122e772487f8a6436854067ef8
lampjin <dlwlsgml3325@gmail.com>
2023-01-04 11:13:50 +0900

    주석처리

--- artagit/src/main/java/com/artagit/web/controller/corporate/ExhibitionController.java
+++ artagit/src/main/java/com/artagit/web/controller/corporate/ExhibitionController.java
@@ -91,9 +91,9 @@ public class ExhibitionController {
 	public String update(Exhibition exhibition, Corporate corporate) {
 //		System.out.println("수정한 전시 ===> "+ id);
 		service.update(exhibition.getId());
-		corporateService.update(id);
-		museumService.update(id);
-		System.out.println(id+"번 전시 수정완료");
+//		corporateService.update(id);
+//		museumService.update(id);
+//		System.out.println(id+"번 전시 수정완료");
 		
 		return "redirect:corp/exh/{id}";
 	}
