9062113b36aa32caa399990cc98163bc7e04bc1d
sungkyoon <harsol@naver.com>
2023-01-06 01:08:15 +0900

    Fix template (index, signup, detail, header, booking-ticket, pay) 적용, Add HomeController

---
+++ artagit/src/main/java/com/artagit/web/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.artagit.web.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("index")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@GetMapping("denied")
+	public String denied() {
+		
+		return "denied";
+	}
+
+
+}
