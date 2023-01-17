0a04ffa801902432f809f02db830b22d61204c3f
HoDdoong <ghqkd333@gmail.com>
2023-01-17 17:39:56 +0900

    고객센터 pc버전 수정

--- artagit/src/main/resources/static/js/customer-center.js
+++ artagit/src/main/resources/static/js/customer-center.js
@@ -1,15 +1,11 @@
 window.addEventListener("load", function(){
-    console.log("h2");
     let section = document.querySelector(".section");
     var qnaBox = section.querySelector(".qna-box");
-    var noticeBox = section.querySelector(".notice-box");
-    // let noticeSection = document.querySelector(".notice-section");
-    // var qnaBox = section.querySelector(".qna-box");
-    // var currentEl = section.querySelector(".active");
+    var noticeBox = section.querySelectorAll(".notice-box");
+    var currentEl = section.querySelector(".active");
 
     qnaBox.onclick = function(e){ 
-        var currentEl = e.target;
-        console.log(currentEl);
+        // var currentEl = e.target;
         
         var isHeader = e.target.nodeName == 'H4'
                         || e.target.classList.contains('accordion-header');
@@ -17,22 +13,44 @@ window.addEventListener("load", function(){
         // if((e.target.nodeName != 'H2') && (e.target.classList.contains("accordion-header")))
         if(!isHeader) // 클릭된 요소가 헤더가 아니면 아무것도 하지 않고 넘어가기.
             return;
+
+            console.log("click!");
+
+
+            if(currentEl != null){
+                console.log(e.target.classList);
+                currentEl.classList.remove("active");
+                currentEl.style.fontWeight = 400;
+                currentEl.style.borderBottom = '1px solid #dbdbdb';
+            } 
+            
+            e.target.classList.add('active');
+            e.target.style.fontWeight = 600;
+            e.target.style.borderBottom = 0;
+            currentEl = e.target;
+            // 닫혀있는 것 열어주기
+            // if(!e.target.classList.contains("active")){
+            //     e.target.classList.add("active");
+            //     prev = e.target;
+            // } else if() {
+            // }
+
+            
             
 
-        console.log(currentEl);
-        console.log("click!");
+        }
+        // if(currentEl!=null || isOpen){
+            //     currentEl.classList.remove('active');
+    //     currentEl = null;
+    // }else{
+    //     e.target.classList.add('active');
+    //     isOpen = true;
+    //     currentEl = e.target;
+    // }
+    for(let i=0; i<noticeBox.length; i++){
 
-        // if(currentEl != null){
-        //     console.log(e.target.classList);
-        //     currentEl.classList.remove("active");
-        // }
-        
-        currentEl.classList.add('active');
-        currentEl = null;
-        
-    }
-
-    noticeBox.onclick = function(e){
+    
+    noticeBox[i].onclick = function(e){
         var isHeader = e.target.nodeName == 'H4'
                         || e.target.classList.contains('accordion-header');
 
@@ -49,6 +67,5 @@ window.addEventListener("load", function(){
         e.target.classList.add('active');
         currentEl = e.target;
     }
+}
 });
-
-// ㅎㅇㅎㅇㅎㅇ
