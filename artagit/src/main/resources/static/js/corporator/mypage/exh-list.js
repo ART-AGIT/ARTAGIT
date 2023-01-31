a8fe5852755ae8f9ab853c5cfacb63619c85cde5
HBEJ <HBEJ9607@gmail.com>
2023-02-01 01:32:47 +0900

    주최자가 등록한 전시페이지 - 전시상태에 맞게 보이게 고침

--- artagit/src/main/resources/static/js/corporator/mypage/exh-list.js
+++ artagit/src/main/resources/static/js/corporator/mypage/exh-list.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
 	//  예매내역리스트 보여주기 (더보기 기능)
 	const itemMore = document.querySelector(".item-more");
-	var page = 1;
+	var page = 0;
 	let today = new Date();
 	var state = null;
 	itemMore.onclick = function () {
@@ -62,7 +62,7 @@ window.addEventListener("load", function () {
 })
 
 
-
+//[주최자가 전시 필터링기능을 눌렀을 때 실행되는 함수]
 function selectexh() {
 	const selectList = document.querySelector("#selectexhibition");
 	const selectBtnTwo = document.querySelector(".select-box");
@@ -77,12 +77,65 @@ function selectexh() {
 
 	console.log(selectBtnTwo.dataset.id);
 
-	// if(selectList.options[selectList.selectedIndex].value == "exh-total"){
-	// 	return 
-	// }
+	if(selectList.options[selectList.selectedIndex].value == "exh-total"){
+	//[전체]눌렀을 때
+	page += 1;
+	let today = new Date();
+	var state = null;
+
+	queryString = `?p=${page}`;
+	console.log(page)
+	fetch(`/corpApi/exh/list${queryString}`)
+		.then((response) => response.json())
+		.then((lists) => {
+			for (list of lists) {
+				console.log(Object.keys(lists).length);
+				if (Object.keys(lists).length < 6)
+					itemMore.classList.add("d-none");
+
+				let date = new Date(list.endDate);
+
+				if (date < today)
+					state = "<p style='color:#545454;'>전시종료</p>";
+
+				else if (date >= today)
+					state = "<p style='color:#FF5171;'>전시중</p>";
+
+				var template = `
+
+					   <div class="exh-list">
+						   <form action="">
+
+							   <div class="exh-img-box">
+								   <a href="/corp/exh/${list.id}" ><img class="exh-img" src="/image/poster/${list.poster}" alt=""></a>
+							   </div>
+							   <div class="text exh-state exh-ing">${state}</div>
+							   <div class="text exh-title">${list.name}</div>
+							   <div class="text exh-author">author. ${list.artist}</div>
+							   <div class="text exh-date" >${list.startDate} ~ ${list.endDate}</div>
+
+							  </form>
+						  </div>
+						  <button class="text item-more"">
+						  <div>더보기</div>
+					   </button>
+					   `;
+
+
+				let el = new DOMParser()
+					.parseFromString(template, "text/html")
+					.body
+					.firstElementChild;
+
+				var list = document.querySelector(".exh-list-tong");
+				list.append(el);
+			}
+		});
+}
+	
 
 	if (selectList.options[selectList.selectedIndex].value == "exh-according") {
-
+		//[전시예정]눌렀을 때
 		queryString = `?p=${page}&s=${selectList.options[selectList.selectedIndex].dataset.id}`;
 		console.log("전시예정");
 		console.log(queryString);
@@ -129,6 +182,7 @@ function selectexh() {
 
 
 	if (selectList.options[selectList.selectedIndex].value == "exh-ing") {
+		//[전시중]눌렀을 때
 		console.log("전시중")
 		queryString = `?p=${page}&s=${selectList.options[selectList.selectedIndex].dataset.id}`;
 		fetch(`/api/exh/list/date${queryString}`)
@@ -172,7 +226,7 @@ function selectexh() {
 			});
 	}
 	if (selectList.options[selectList.selectedIndex].value == "exh-end") {
-		
+		//[전시종료]눌렀을 때
 		console.log("전시종료")
 		queryString = `?p=${page}&s=${selectList.options[selectList.selectedIndex].dataset.id}`;
 		console.log(page)
@@ -181,7 +235,7 @@ function selectexh() {
 		.then((response) => response.json())
 		.then((lists) => {
 			
-			console.log(lists)
+			console.log("리스트입니다"+lists.length)
 				listTong.innerHTML = '';
 				for (list of lists) {
 
