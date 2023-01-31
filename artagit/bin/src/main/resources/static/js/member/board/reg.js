ddfb7cded020460875d8530776b58eda97f3fd3e
HBEJ <HBEJ9607@gmail.com>
2023-01-31 16:53:12 +0900

    충돌 수정 중

---
+++ artagit/bin/src/main/resources/static/js/member/board/reg.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function(){
+    const imgInput = document.querySelector(".input-image-button1")
+	const fileInput = document.querySelector(".input-image1");
+	var toolbox = document.querySelector(".toolbox");
+
+
+	imgInput.onclick = function(e) {
+
+		let event = new MouseEvent("click", {
+			'view': window,
+			'bubble': true,
+			'cancelable': true
+		});
+
+		fileInput.dispatchEvent(event);
+	}
+
+	fileInput.oninput = function(e) {
+		let url = fileInput.files[0];
+		let reader = new FileReader();
+		reader.onload = (evt) => {
+			imgInput.src = evt.target.result;
+		};
+		reader.readAsDataURL(url);
+	}
+
+	
+	})
